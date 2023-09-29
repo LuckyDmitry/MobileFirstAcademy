5f8df0be5fa73ed6fc93364807025330e716d0ef
Dmitrii Trifonov <dmitriy.trifonov@tabby.ai>
2023-09-29 22:36:53 +0200

    fix

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,21 +1,28 @@
+import Foundation
 import Vapor
-import telegram_vapor_bot
+import TelegramVaporBot
 
 let tgApi: String = "6404683601:AAFRr8n4sGmkQz0sD4q1aLLC2M51e3Myrkw"
 
-// configures your application
-public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    app.http.server.configuration.hostname = "0.0.0.0"
-    app.http.server.configuration.port = 80
-    
-    let connection: TGConnectionPrtcl = TGLongPollingConnection()
-    TGBot.configure(connection: connection, botId: tgApi, vaporClient: app.client)
-    try TGBot.shared.start()
-    TGBot.log.logLevel = .error
-    DefaultBotHandlers.addHandlers(app: app, bot: TGBot.shared)
+actor TGBotConnection {
+    private var _connection: TGConnectionPrtcl!
 
-    // register routes
+    var connection: TGConnectionPrtcl {
+        self._connection
+    }
+    
+    func setConnection(_ conn: TGConnectionPrtcl) {
+        self._connection = conn
+    }
+}
+
+public func configure(_ app: Application) async throws {
+    /// set level of debug if you needed
+    TGBot.log.logLevel = app.logger.logLevel
+    let bot: TGBot = .init(app: app, botId: tgApi)
+    await TGBOT.setConnection(try await TGLongPollingConnection(bot: bot))
+    await DefaultBotHandlers.addHandlers(app: app, connection: TGBOT.connection)
+    try await TGBOT.connection.start()
+
     try routes(app)
 }
